f059982d41bee6644d00dbbd8fda9686c079c235
Carlos Daniel <20032000dani@gmail.com>
2024-05-28 16:37:32 +0200

    try of tag

--- romans.js
+++ romans.js
@@ -32,6 +32,9 @@ function init() {
     var convertion = modeCheckbox.checked ? convertIntegerToRoman(inputValue) : convertRomanToInteger(inputValue);
     if (convertion.result) {
       outputArea.innerHTML = convertion.value;
+      gtag('event', 'convert_calculated', {
+        'converValue': convertion.value
+      });
     } else {
       alert(convertion.message);
     }
