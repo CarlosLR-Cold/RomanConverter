10cb55c868b50dd89b8f1b12d5d8f23a577f6851
Carlos Daniel <20032000dani@gmail.com>
2024-05-28 16:41:14 +0200

    try index

--- romans.js
+++ romans.js
@@ -32,9 +32,6 @@ function init() {
     var convertion = modeCheckbox.checked ? convertIntegerToRoman(inputValue) : convertRomanToInteger(inputValue);
     if (convertion.result) {
       outputArea.innerHTML = convertion.value;
-      gtag('event', 'convert_calculated', {
-        'converValue': convertion.value
-      });
     } else {
       alert(convertion.message);
     }
